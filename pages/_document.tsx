6128f057c6beee78b74ef3a42b0e68701ef368d4
Rafael Pereira <rafaelhpmg@gmail.com>
2022-08-05 17:59:41 -0300

    :lipstick: Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
         <meta name="description" content="Gainz" />
         <meta name="keywords" content="Gainz, IMC, TMB, BF" />
         <meta name="author" content="Rafael DEV" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
